8e430be37866adb967078651a6fd1d26b0d5d46d
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-09-10 21:59:16 +0000

    Adding additional exercises

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOfBirth, currentYear){
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1991, 2018));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
